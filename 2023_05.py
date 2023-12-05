daae5096205ff0f6ccbec89d6900896e11256c61
Your Name <you@example.com>
2023-12-05 18:35:37 +0900

    cleanup

--- 2023_05.py
+++ 2023_05.py
@@ -1,5 +1,4 @@
 from aocd.models import Puzzle
-from aocd import submit
 import os
 from collections import deque
 # import puzzle
@@ -10,7 +9,19 @@ puzzle = Puzzle(year=int(year), day=int(day))
 
 def parse(puzzle_input):
     """Parse input."""
-    return [line for line in puzzle_input.split("\n")]
+    data = [line for line in puzzle_input.split("\n")]
+    tables = {}
+    _, rest = data[0].split(":")
+    seeds = rest.split()
+    for line in data[2:]:
+        if len(line) <= 3:
+            continue
+        if ":" in line:
+            category, _ = line.split(" ")
+            tables[category] = []
+        else:
+            tables[category].append([int(x) for x in line.split(" ")])
+    return seeds, tables
 
 
 def table_to_value(table, value):
@@ -24,31 +35,14 @@ def table_to_value(table, value):
 
 def part1(data):
     """Solve part 1."""
-    tables = {}
-    _, rest = data[0].split(":")
-    seeds = rest.split()
-    for line in data[2:]:
-        if len(line) <= 3:
-            continue
-        if ":" in line:
-            category, _ = line.split(" ")
-            tables[category] = []
-        else:
-            tables[category].append([int(x) for x in line.split(" ")])
-    min_loc = None
+    seeds, tables = data
+    min_loc = float('inf')
 
     for seed in seeds:
-        soil = table_to_value(tables['seed-to-soil'], int(seed))
-        fertilizer = table_to_value(tables['soil-to-fertilizer'], soil)
-        water = table_to_value(tables['fertilizer-to-water'], fertilizer)
-        light = table_to_value(tables['water-to-light'], water)
-        temperature = table_to_value(tables['light-to-temperature'], light)
-        humidity = table_to_value(
-            tables['temperature-to-humidity'], temperature)
-        location = table_to_value(tables['humidity-to-location'], humidity)
-
-        if not min_loc or min_loc > location:
-            min_loc = location
+        location = table_to_value(tables['seed-to-soil'], int(seed))
+        for table_name in ['soil-to-fertilizer', 'fertilizer-to-water', 'water-to-light', 'light-to-temperature', 'temperature-to-humidity', 'humidity-to-location']:
+            location = table_to_value(tables[table_name], location)
+        min_loc = min(min_loc, location)
 
     return min_loc
 
@@ -85,50 +79,20 @@ def table_to_ranges(table, ranges):
         else:
             return_val.append(range)
 
-    print(count_range(return_val))
     return return_val
 
 
 def part2(data):
     """Solve part 2."""
-    tables = {}
-    _, rest = data[0].split(":")
-    seeds = rest.split()
-    for line in data[2:]:
-        if len(line) <= 3:
-            continue
-        if ":" in line:
-            category, _ = line.split(" ")
-            tables[category] = []
-        else:
-            tables[category].append([int(x) for x in line.split(" ")])
+    seeds, tables = data
 
-    min_loc = None
-    ranges = []
-    for i in range(int(len(seeds)/2)):
-        ranges.append(
-            [int(seeds[i*2]), int(seeds[i*2]) + int(seeds[2*i+1]) - 1])
+    ranges = [[int(seeds[i]), int(seeds[i]) + int(seeds[i+1]) - 1]
+              for i in range(0, len(seeds), 2)]
 
-    ranges = table_to_ranges(tables['seed-to-soil'], ranges)
-    ranges = table_to_ranges(tables['soil-to-fertilizer'], ranges)
-    ranges = table_to_ranges(tables['fertilizer-to-water'], ranges)
-    ranges = table_to_ranges(tables['water-to-light'], ranges)
-    ranges = table_to_ranges(tables['light-to-temperature'], ranges)
-    ranges = table_to_ranges(tables['temperature-to-humidity'], ranges)
-    ranges = table_to_ranges(tables['humidity-to-location'], ranges)
+    for table_name in ['seed-to-soil', 'soil-to-fertilizer', 'fertilizer-to-water', 'water-to-light', 'light-to-temperature', 'temperature-to-humidity', 'humidity-to-location']:
+        ranges = table_to_ranges(tables[table_name], ranges)
 
-    for r in ranges:
-        if not min_loc or min_loc > r[0]:
-            min_loc = r[0]
-
-    return min_loc
-
-
-def count_range(ranges):
-    sum = 0
-    for r in ranges:
-        sum += r[1]-r[0] + 1
-    return sum
+    return min(r[0] for r in ranges)
 
 
 def solve(puzzle_input):
@@ -140,51 +104,7 @@ def solve(puzzle_input):
     return f"{solution1}", f"{solution2}"
 
 
-def test_table_to_ranges():
-    table = [
-        [100, 10, 5]
-    ]
-    ranges = [[1, 20]]
-    expected_output = [[100, 104], [1, 9], [15, 20]]
-    result = table_to_ranges(table, ranges)
-    assert result == expected_output
-
-    table = [
-        [100, 10, 5]
-    ]
-    ranges = [[11, 20]]
-    expected_output = [[101, 104], [15, 20]]
-    result = table_to_ranges(table, ranges)
-    assert result == expected_output
-
-    table = [
-        [100, 10, 5]
-    ]
-    ranges = [[11, 12]]
-    expected_output = [[101, 102]]
-    result = table_to_ranges(table, ranges)
-    assert result == expected_output
-
-    table = [
-        [100, 10, 5]
-    ]
-    ranges = [[1, 12]]
-    expected_output = [[100, 102], [1, 9]]
-    result = table_to_ranges(table, ranges)
-    assert result == expected_output
-
-    table = [
-        [100, 10, 5]
-    ]
-    ranges = [[1, 12], [13, 20]]
-    expected_output = [[100, 102], [103, 104], [1, 9], [15, 20]]
-    result = table_to_ranges(table, ranges)
-    assert result == expected_output
-
-
 if __name__ == "__main__":
-    test_table_to_ranges()
-
     for example in puzzle.examples:
         result_a, result_b = solve(example.input_data)
         if result_a != example.answer_a:
