416ecd23d9867a49d39e5ce4a711bd00b3d60c6d
Your Name <you@example.com>
2023-12-11 09:33:41 +0900

    add skelingtons

---
+++ 2023_23.py
@@ -0,0 +1,50 @@
+from aocd.models import Puzzle
+from aocd import submit
+import os
+
+# import puzzle
+filename = os.path.basename(__file__)
+year, day = filename[:-3].split("_")[:2]
+puzzle = Puzzle(year=int(year), day=int(day))
+
+
+def parse(puzzle_input):
+    """Parse input."""
+    return [int(line) for line in puzzle_input.split()]
+
+
+def part1(data):
+    """Solve part 1."""
+    return None
+
+
+def part2(data):
+    """Solve part 2."""
+    return None
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return f"{solution1}", f"{solution2}"
+
+
+if __name__ == "__main__":
+    for example in puzzle.examples:
+        result_a, result_b = solve(example.input_data)
+        if result_a != example.answer_a:
+            print(f"Expected {example.answer_a}, got {result_a}")
+            raise ValueError("Test case failed for Part A")
+
+        if example.answer_b is not None:
+            if result_b != example.answer_b:
+                print(f"Expected {example.answer_b}, got {result_b}")
+                raise ValueError("Test case failed for Part B")
+
+    answer_a, answer_b = solve(puzzle.input_data)
+    puzzle.answer_a = answer_a
+    if answer_b != "None":
+        puzzle.answer_b = answer_b
