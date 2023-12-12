73abecf90aa5e40981285e809098361fb50ac7e2
Your Name <you@example.com>
2023-12-12 18:15:03 +0900

    clean up

--- 2023_12.py
+++ 2023_12.py
@@ -14,38 +14,19 @@ def parse(puzzle_input):
     return [line for line in puzzle_input.split("\n")]
 
 
-def do_rec_magic(springs, conditions):
-    arrangements = []
-    arrangements.append(springs)
-    while conditions:
-        condition = conditions.pop(0)
-        new_ar = []
-        for ar in arrangements:
-            # s = r"(?=([\?#]{" + re.escape(condition) + r"}))"
-            s = r"(?=([\?#]{" + re.escape(condition) + r"}))"
-            for match in re.finditer(s, ar):
-                if len(ar) >= match.end(1) and "#" not in ar[0:max(0, match.start(1))] and (len(ar) == match.end(1) or ar[match.end(1)] != "#"):
-                    new_ar.append(ar[match.end(1) + 1::])
-        arrangements = new_ar
-    arrangements = [ar for ar in arrangements if "#" not in ar]
-    return len(arrangements)
-
-
-def do_rec_magic2(springs, conditions):
+def compute(springs, conditions):
     arrangements = defaultdict(lambda: 0)
     arrangements[springs] += 1
-    while conditions:
-        condition = conditions.pop(0)
+    for condition in conditions:
         new_ar = defaultdict(lambda: 0)
-        for ar in arrangements.keys():
-            # s = r"(?=([\?#]{" + re.escape(condition) + r"}))"
-            s = r"(?=([\?#]{" + re.escape(condition) + r"}))"
+        for ar in arrangements:
+            s = rf'(?=([\?#]{{{re.escape(condition)}}})([^#]+|$))'
             for match in re.finditer(s, ar):
-                if len(ar) >= match.end(1) and "#" not in ar[0:max(0, match.start(1))] and (len(ar) == match.end(1) or ar[match.end(1)] != "#"):
+                if len(ar) >= match.end(1) and "#" not in ar[0:max(0, match.start(1))]:
                     new_ar[ar[match.end(1) + 1::]] += arrangements[ar]
         arrangements = new_ar
-    arrangements = sum([v for k, v in arrangements.items() if "#" not in k])
-    return arrangements
+
+    return sum(v for k, v in arrangements.items() if "#" not in k)
 
 
 def part1(data):
@@ -53,9 +34,8 @@ def part1(data):
     result = 0
     for line in data:
         springs, conditions = line.split()
-        # springs = [char for char in springs]
         conditions = conditions.split(",")
-        result += do_rec_magic2(springs, conditions)
+        result += compute(springs, conditions)
     return result
 
 
@@ -64,16 +44,10 @@ def part2(data):
     result = 0
     for line in data:
         springs, conditions = line.split()
-        springs = springs + "?" + springs + "?" + \
-            springs + "?" + springs + "?" + springs
-        conditions = conditions + "," + conditions + "," + \
-            conditions + "," + conditions + "," + conditions
-        # springs = [char for char in springs]
-        # conditions = [int(i) for i in conditions.split(",")]
-        # springs = [len(s) for s in springs.split(".")]
-        conditions = conditions.split(",")
-        result += do_rec_magic2(springs, conditions)
-        print(result)
+        springs = '?'.join([springs] * 5)
+        conditions = [condition for _ in range(
+            5) for condition in conditions.split(',')]
+        result += compute(springs, conditions)
     return result
 
 
