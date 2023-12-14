9e667c6a8508cefcf324906a97e419d8280475f6
Your Name <you@example.com>
2023-12-14 15:28:38 +0900

    first version

--- 2023_14.py
+++ 2023_14.py
@@ -10,17 +10,104 @@ puzzle = Puzzle(year=int(year), day=int(day))
 
 def parse(puzzle_input):
     """Parse input."""
-    return [int(line) for line in puzzle_input.split()]
+    return [line for line in puzzle_input.split()]
+
+
+def transpose(matrix):
+    return ["".join([str(matrix[i][j]) for i in range(len(matrix))]) for j in range(len(matrix[0]))]
+
+
+def calculate_weight(data):
+    """Solve part 1."""
+    rows = transpose(data)
+    total_weight = 0
+    for row in rows:
+        row = list(row)
+        for i, cell in enumerate(row):
+            if cell == "O":
+                total_weight += len(row) - i
+
+    return total_weight
 
 
 def part1(data):
     """Solve part 1."""
-    return None
+    rows = transpose(data)
+    total_weight = 0
+    for row in rows:
+        row = list(row)
+        for i, cell in enumerate(row):
+            if cell == "#":
+                continue
+            if cell == "O":
+                j = i - 1
+                while j >= 0 and row[j] == ".":
+                    row[j], row[j+1] = row[j+1], row[j]
+                    j -= 1
+        for i, cell in enumerate(row):
+            if cell == "O":
+                total_weight += len(row) - i
+
+    return total_weight
+
+
+def m_transpose(matrix):
+    return [[str(matrix[i][j]) for i in range(len(matrix))] for j in range(len(matrix[0]))]
+
+
+def roll(data):
+    for row in data:
+        for i, cell in enumerate(row):
+            if cell == "#":
+                continue
+            if cell == "O":
+                j = i - 1
+                while j >= 0 and row[j] == ".":
+                    row[j], row[j+1] = row[j+1], row[j]
+                    j -= 1
+    return data
+
+
+def do_cycle(data):
+    north = m_transpose(data)
+    north = roll(north)
+    north = m_transpose(north)
+    west = roll(north)
+    south = list(reversed(west))
+    south = m_transpose(south)
+    south = roll(south)
+    south = m_transpose(south)
+    south = list(reversed(south))
+    east = [list(reversed(r)) for r in south]
+    east = roll(east)
+    east = [list(reversed(r)) for r in east]
+    return east
+
+    return data
 
 
 def part2(data):
     """Solve part 2."""
-    return None
+    data = [[c for c in l] for l in data]
+    previous = list()
+    result = ""
+    for i in range(1000000000):
+        data = do_cycle(data)
+        result = "".join(cell for row in data for cell in row)
+        if result in previous:
+            break
+        else:
+            previous.append(result)
+    index = previous.index(result)
+    cycle_length = len(previous) - index
+    final_index = ((1000000000 - index) % cycle_length) + index - 1
+    print(previous[final_index])
+    print(len(previous), index, final_index)
+    s = previous[final_index]
+    s = [s[i:i + len(data[0])] for i in range(0, len(s), len(data[0]))]
+
+    weight = calculate_weight(s)
+    return weight
 
 
 def solve(puzzle_input):
@@ -40,8 +127,8 @@ if __name__ == "__main__":
             raise ValueError("Test case failed for Part A")
 
         if example.answer_b is not None:
-            if result_b != example.answer_b:
-                print(f"Expected {example.answer_b}, got {result_b}")
+            if result_b != "64":
+                print(f"Expected 64, got {result_b}")
                 raise ValueError("Test case failed for Part B")
 
     answer_a, answer_b = solve(puzzle.input_data)
