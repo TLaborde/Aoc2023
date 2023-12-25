258ca41593a9ded0f82ddf578b4af66a005c0e31
TLaborde <johndoe@example.com>
2023-12-25 09:40:45 +0100

    should rezrite polyfit...

--- 2023_21.py
+++ 2023_21.py
@@ -10,17 +10,98 @@ puzzle = Puzzle(year=int(year), day=int(day))
 
 def parse(puzzle_input):
     """Parse input."""
-    return [int(line) for line in puzzle_input.split()]
+    return [[c for c in line ]for line in puzzle_input.split()]
 
+def get_nei(points, map):
+    nei = set()
+    for (x,y) in points:
+        x +=1
+        if x < len(map) and map[x][y] == '.' and (x,y) not in nei:
+            nei.add((x,y))
+        x -= 2
+        if x >= 0 and map[x][y] == '.' and (x,y) not in nei:
+            nei.add((x,y))
+        x +=1
+        y += 1
+        if y < len(map[0]) and map[x][y] == '.' and (x,y) not in nei:
+            nei.add((x,y))
+        y -= 2
+        if y >= 0 and map[x][y] == '.' and (x,y) not in nei:
+            nei.add((x,y))
+    return nei
+
+def get_infinite_nei(points, map):
+    nei = set()
+    for (x,y) in points:
+        x +=1
+        if map[x%len(map)][y%len(map[0])] in ['.','S'] and (x,y) not in nei:
+            nei.add((x,y))
+        x -= 2
+        if map[x%len(map)][y%len(map[0])] in ['.','S'] and (x,y) not in nei:
+            nei.add((x,y))
+        x +=1
+        y += 1
+        if map[x%len(map)][y%len(map[0])] in ['.','S'] and (x,y) not in nei:
+            nei.add((x,y))
+        y -= 2
+        if map[x%len(map)][y%len(map[0])] in ['.','S'] and (x,y) not in nei:
+            nei.add((x,y))
+    return nei
 
 def part1(data):
-    """Solve part 1."""
-    return None
+    knowns = set()
+    to_discover = set()
+    for i, line in enumerate(data):
+        for j, c in enumerate(line):
+            if c == "S":
+                to_discover.add((i,j))
+    if len(data) == 11:
+        r = 3
+    else:
+        r = 32
+    for _ in range(r):
+        nei = get_nei(get_nei(to_discover, data),data)
+        to_discover = [n for n in nei if n not in knowns] 
+        for t in nei:
+            if t not in knowns:
+                knowns.add(t)
+    return len(knowns)+1
 
+def reachable(data, r):
+    knowns = set()
+    to_discover = set()
+    for i, line in enumerate(data):
+        for j, c in enumerate(line):
+            if c == "S":
+                to_discover.add((i,j))
+
+    for _ in range(r):
+        nei = get_infinite_nei(to_discover, data)
+        to_discover = [n for n in nei if n not in knowns] 
+        for t in nei:
+            if t not in knowns:
+                knowns.add(t)
+    knowns = [(x,y) for (x,y) in knowns if (x +y)% 2 == r %2 ]
+    return len(knowns)
 
 def part2(data):
-    """Solve part 2."""
-    return None
+    if len(data) == 11:
+        steps = [6,10,50,100,500]
+    else:
+        steps = [65+ 131, 65+ 131*2, 65+ 131*3]
+
+    plots = [reachable(data, s) for s in steps]
+
+    print(plots)
+    import numpy as np
+    # hours = [6, 10,50,100,500,1000,5000]
+    # happ = [16,50,1594,6536,167004,668697,16733044]
+    # #polynomial fit with degree = 2
+    model = np.poly1d(np.polyfit(steps, plots, 2))
+    print(model)
+    # #add fitted polynomial line to scatterplot
+    print(model(26501365))
+    return int(model(26501365))
 
 
 def solve(puzzle_input):
@@ -35,7 +116,7 @@ def solve(puzzle_input):
 if __name__ == "__main__":
     for example in puzzle.examples:
         result_a, result_b = solve(example.input_data)
-        if result_a != example.answer_a:
+        if result_a != '16':
             print(f"Expected {example.answer_a}, got {result_a}")
             raise ValueError("Test case failed for Part A")
 
